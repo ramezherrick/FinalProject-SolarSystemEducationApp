9a684d314219a37f7d36b4bc413e9efb2937d1e1
Josh <jabourjosh@gmail.com>
2020-09-14 09:08:55 -0400

    add/remove questions method in teacher controller along with views

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -64,5 +64,82 @@ namespace FinalProject_SolarSystemEducationApp.Controllers
             var questionsbank = _context.Questionsbank;
             return View(questionsbank);
         }
+
+        public IActionResult DisplayQB()
+        {
+            _context.Questions.ToList();
+            var quizes = _context.Quizes.ToList();
+            var qb = _context.Questionsbank.ToList();
+
+            ViewBag.Quizes = quizes;
+
+            return View(qb);
+        }
+
+        //ADD QUESTION TO Questions table and ASSIGN QuizId & QuestionID to Questionsbank
+        [HttpGet]
+        public IActionResult AddQuestionToQB()
+        {
+            var quizes = _context.Quizes.ToList();
+            ViewBag.questions = _context.Questions.ToList();
+            return View(quizes);
+        }
+
+        [HttpPost]
+        public IActionResult AddQuestionToQB(Questions newQuestion, int quizId)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add new question to DB
+                _context.Questions.Add(newQuestion);
+                _context.SaveChanges();
+
+                //create relation between quizes and questions
+                var newQB = new Questionsbank();
+                newQB.QuestionId = newQuestion.Id;
+                newQB.QuizId = quizId;
+
+                //Add new relationship to DB
+                _context.Questionsbank.Add(newQB);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("AddQuestionToQB");
+        }
+
+        public async Task<IActionResult> RemoveQuestion(int id)
+        {
+            //Remove constraint from question bank 1st
+            List<Questionsbank> lqb = _context.Questionsbank.ToList();
+            Questionsbank qbRemove = new Questionsbank();
+
+            foreach (Questionsbank qb in lqb)
+            {
+                if (qb.QuestionId == id)
+                {
+                    qbRemove = qb;
+                }
+            }
+            
+            if (qbRemove != null)
+            {
+                _context.Questionsbank.Remove(qbRemove);
+                //_context.SaveChanges();
+                await _context.SaveChangesAsync();
+            }
+
+            //remove question from question table 2nd
+            //Questions currentQuestion = _context.Questions.Find(id);
+            Questions currentQuestion = await _context.Questions.FindAsync(id);
+
+            if (currentQuestion != null)
+            {
+                _context.Questions.Remove(currentQuestion);
+                //_context.SaveChanges();
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("AddQuestionToQB");
+        }
     }
 }
