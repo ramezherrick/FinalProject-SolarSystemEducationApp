00cb07525da0b71531653dd2d5113e8a1e55d3c1
Josh <jabourjosh@gmail.com>
2020-09-15 09:28:39 -0400

    Added display bodies view/details view. Cleaned up multiple other pages, added buttons to layout

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject_SolarSystemEducationApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject_SolarSystemEducationApp.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly SolarDAL _solarDAL;
+
+        public StudentController()
+        {
+            _solarDAL = new SolarDAL();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> DisplayBodies()
+        {
+            var allInfo = await _solarDAL.GetBody();
+            return View(allInfo);
+        }
+
+        public async Task<IActionResult> BodyDetails(string id)
+        {
+            List<Body> bodiesList = await _solarDAL.GetBody();
+            Body currentBody = new Body();
+            foreach (Body b in bodiesList)
+            {
+                if (b.id == id)
+                {
+                    currentBody = b;
+                }
+            }
+
+            return View(currentBody);
         }
     }
 }
